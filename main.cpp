9fed5993b7701d23520dbbb1e9bf81a9fcb5591d
JFonS <joan.fonssanchez@gmail.com>
2014-12-02 10:45:26 +0100

    gg

--- main.cpp
+++ main.cpp
@@ -1,18 +1,23 @@
-#include <curlpp/cURLpp.hpp>
+/*#include <curlpp/cURLpp.hpp>
 #include <curlpp/Easy.hpp>
-#include <curlpp/Options.hpp>
+#include <curlpp/Options.hpp>*/
 
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <unistd.h>
 
 #define HEADER_ACCEPT "Accept:text/html,application/xhtml+xml,application/xml"
 #define HEADER_USER_AGENT "User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.70 Safari/537.17"
-
+#define SAVE_FILE "eda-bot-save.txt"
+#define NO_ROUND "AJAHBDBJbgajFbIfFrDdHkBbcSdRWDghOpJb"
 using namespace std;
 
-void curlpp_init(curlpp::Easy &request, ostringstream &responseStream) {
+/*curlpp::Easy request;
+ostringstream responseStream;
+
+void curlpp_init() {
   list<string> headers;
   headers.push_back(HEADER_ACCEPT);
   headers.push_back(HEADER_USER_AGENT);
@@ -26,22 +31,25 @@ void curlpp_init(curlpp::Easy &request, ostringstream &responseStream) {
 }
 
 int read_last_round() {
-  fstream sav;
-  
-  
+  ifstream isav(SAVE_FILE);
+  int n;
+  if (isav.good()) {
+    isav >>  n;
+  }  else {
+    n = 1;
+    while (get_round(n) !=  NO_PLAYER) ++n;
+    ofstream osav;
+    osav.open(SAVE_FILE);
+    osav <<  n;
+    osav.close();
+  }
+  isav.close();
+  return n;
 }
 
-
-int main() {
-  curlpp::Easy request;
-  ostringstream responseStream;
-  curlpp_init(request, responseStream);
-
-
-
-  for (int i = 1; i < 14; ++i) {
-
-    char buff[100];
+string get_round(int i) {
+  string ret =  NO_ROUND;
+  char buff[100];
     sprintf(buff, "https://www.jutge.org/competitions/AlbertAtserias:EDA_2014_2015_Q1/round/%d", i);
     string url = buff;
     request.setOpt(curlpp::options::Url(url));
@@ -53,15 +61,29 @@ int main() {
       size_t inici = re.find(found+1,"++++++");
       size_t fi =  re.find(found+1,"------");
       if (inici != string::n_pos and fi != string::n_pos) {
-        string dead =  re.substr(inici+offset1, fi-offset2);
-        cout <<  dead << " is out" <<  endl;
+        ret =  re.substr(inici+offset1, fi-offset2);
       } else {
-        cout << "NOVA RONDA (" << i << ")" << endl;
+        ret =  "";
       }
-    } else {
-      cout << "NOT YET (" << i << ")" << endl;
     }
     responseStream.str(string());
+    return ret;
+}*/
+
+
+int main() {
+  //curlpp_init();
+  int last_round = 7;//  read_last_round();
+  
+  while(true) {
+    string res = NO_ROUND;// get_round(last_round + 1);
+    if (res !=  NO_ROUND) {
+      //send_twit(res);
+      ++last_round;
+    } else {
+      cout <<  "lolo";
+      usleep(1000000);
+    }
   }
 
   return 0;
